72ec26a727e48eb52e21678eb58801874d4ac494
Ranjo8192 <34912190+ranjo8192@users.noreply.github.com>
2018-01-31 18:08:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var dbConnection =require('./mongoDbConnection');
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: "sshhh",
+    name: "expresscookie",
+    //store: sessionStore, // connect-mongo session store
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+var userSession; 
+
+
+
+
+app.get('/', function(req, res){
+	res.render('pages/login');
+});
+
+/*****************Login Starts***************************/
+app.post('/userLogin', function(req,res){
+	var userSession = req.session;
+	username = req.body.username;
+	upassword = req.body.password;
+	console.log(username);
+	console.log(upassword);
+	console.log(userSession);
+
+	var con = connect.mongoDbConnection.dbConnection();
+	con.connect(function(err){
+		if(err){
+			throw err;
+		}
+		else {
+			console.log("connection is imported successfully");
+			res.end();
+		}
+	})
+
+	
+});
+/*****************Login Ends*****************************/
+app.get('/register', function(req, res){
+	res.render('pages/register');
+});
+
+
+
+var server = app.listen(30000, function(req,res){
+	console.log("Server is listening at http://localhost:30000/");
+});
