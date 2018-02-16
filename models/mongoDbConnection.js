d49be9328257c3a79edfed945d3bcbf1aef7109a
Ranjo8192 <34912190+ranjo8192@users.noreply.github.com>
2018-02-16 12:12:49 +0530

    Add files via upload

---
+++ models/mongoDbConnection.js
@@ -0,0 +1,16 @@
+var mongodb = require('mongodb');
+var MongoClent = mongodb.MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+exports.dbConnection = function(){
+var con = MongoClient.connect(url, function(err,db){
+	if (err){
+		throw err;
+	}
+	else {
+		console.log("connection with database has been stablished successfully..!");
+		db.close();
+	}
+});
+return con;
+};
